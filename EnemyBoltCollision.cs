3c7645ca11dc4878d936fc0eae0e382fb740024e
WishPenguin <143478568+WishPenguin@users.noreply.github.com>
2024-11-05 14:15:35 +1100

    Update EnemyBoltCollision.cs

--- EnemyBoltCollision.cs
+++ EnemyBoltCollision.cs
@@ -9,7 +9,7 @@ public class EnemyBoltCollision : MonoBehaviour
 
     private GameObject launchingAsteroid;
 
-    // Method to set the launching asteroid
+   
     public void SetLaunchingAsteroid(GameObject asteroidShooter)
     {
         launchingAsteroid = asteroidShooter;
@@ -17,32 +17,27 @@ public class EnemyBoltCollision : MonoBehaviour
 
     private void Start()
     {
-        // Start a coroutine to ignore collisions briefly
         StartCoroutine(IgnoredCollisionCoroutine());
     }
 
     private IEnumerator IgnoredCollisionCoroutine()
     {
-        // Wait for a short time before enabling collisions
         yield return new WaitForSeconds(0.1f);
         Physics.IgnoreCollision(GetComponent<Collider>(), launchingAsteroid.GetComponent<Collider>());
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        // Ignore collisions with Boundary and ForceField
         if (other.CompareTag("Boundary") || other.CompareTag("ForceField"))
         {
             return;
         }
 
-        // Check if the collided object is the launching asteroid
         if (other.gameObject == launchingAsteroid)
         {
             return; // Ignore collision with the launching asteroid
         }
 
-        // Instantiate explosion effects
         Instantiate(explosion, other.transform.position, transform.rotation);
 
         if (other.CompareTag("Player"))
@@ -50,7 +45,6 @@ public class EnemyBoltCollision : MonoBehaviour
             Instantiate(Playerexplosion, other.transform.position, other.transform.rotation);
         }
 
-        // Destroy the collided object and the bolt itself
         Destroy(other.gameObject);
         Destroy(gameObject);
     }

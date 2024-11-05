b0b44725710579f586f0cacfce5ad17a6bde7351
WishPenguin <143478568+WishPenguin@users.noreply.github.com>
2024-11-05 14:16:07 +1100

    Update DestroyByContact.cs

--- DestroyByContact.cs
+++ DestroyByContact.cs
@@ -9,19 +9,19 @@ public class DestroyByContact : MonoBehaviour
 
     void OnTriggerEnter(Collider other)
     {
-        // Ignore collisions with Boundary and ForceField
+      
         if (other.tag == "Boundary" || other.tag == "ForceField")
         {
             return;
         }
 
-        // Prevent destroying the asteroid the bolt came from
+     
         if (other.CompareTag("AsteroidShooter"))
         {
             return; // Adjust this tag based on your asteroid's tag
         }
 
-        // Instantiate explosion effects
+      
         Instantiate(explosion, other.transform.position, transform.rotation);
 
         if (other.CompareTag("Player"))
@@ -29,7 +29,7 @@ public class DestroyByContact : MonoBehaviour
             Instantiate(Playerexplosion, other.transform.position, other.transform.rotation);
         }
 
-        // Destroy the collided object and the bolt itself
+      
         Destroy(other.gameObject);
         Destroy(gameObject);
     }
